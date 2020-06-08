6cdcaa1c222ad296adc3091a6f73fad7b317bc1f
honeybaey <honeybaey@gmail.com>
2020-06-08 17:42:51 +0300

    adding logic

--- index.js
+++ index.js
@@ -1,69 +1,109 @@
-// operations
-function sum(a, b) {
-  return a + b;
-}
-function subtraction(a, b) {
-  return a - b;
-}
-function multiply(a, b) {
-  return a * b;
-}
-function divide(a, b) {
-  return a / b;
-}
-function operate(a, b, operator) {
-  return operator(a, b);
-}
-
-// function calc() {
-//   const display = document.getElementById("display");
-//   switch (display.value) {
-//     case "+":
-//       result = a + b;
-//       break;
-//     case "-":
-//       result = a - b;
-//       break;
-//     case "*":
-//       result = a * b;
-//       break;
-//     case "÷":
-//       result = a / b;
-//   }
-// }
-
 function calc() {
+  const display = easyToSearch("#display");
+  const clearButton = easyToSearch("#clear");
+  const removeButton = easyToSearch("#remove");
+  const equal = easyToSearch("#equal");
+  const ops = easyToSearch(".ops");
+  const nums = easyToSearch(".num");
 
-}
+  let num = "";
+  let oldNum = "";
+  let resultNum;
+  let operator;
 
-function showNum() {
-  const display = document.getElementById("display");
-  const container = document.querySelector(".container");
-  container.addEventListener("click", clickButton);
-  let displayCounter = "";
+  // функция удобного поиска элемента
+  function easyToSearch(element) {
+    if (element.charAt(0) === "#") {
+      return document.querySelector(element);
+    }
 
-  function clickButton(e) {
-    const button = e.target;
-    if (button.tagName === "BUTTON") {
-      displayCounter = display.value += button.value;
-      console.log(displayCounter);
+    return document.querySelectorAll(element);
+  }
+
+  // функция назначения операторов
+  function setOps() {
+    if (!operator) {
+      oldNum = num;
+      num = "";
+      operator = this.value;
     }
   }
+
+  for (let i = 0; i < ops.length; i++) {
+    ops[i].onclick = setOps;
+  }
+
+  // функция отображения цифр на дисплее
+  function setNums() {
+    num += this.value;
+    display.value = num;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    nums[i].onclick = setNums;
+  }
+
+  // функция полного очищения дисплея
+  function onClearButton() {
+    display.value = "";
+    oldNum = "";
+    num = "";
+  }
+  clearButton.addEventListener("click", onClearButton);
+
+  // функция удаления последней цифры на дисплее
+  function onRemoveButton() {
+    display.value = display.value.slice(0, -1);
+    oldNum = display.value;
+    num = display.value;
+  }
+  removeButton.addEventListener("click", onRemoveButton);
+
+  // if (display.value.includes(".")) {
+  //   console.log("Много точек");
+  // }
+
+  // логика операций на дисплее
+  function displayNum() {
+    oldNum = parseFloat(oldNum);
+    num = parseFloat(num);
+
+    switch (operator) {
+      case "+":
+        resultNum = oldNum + num;
+        break;
+
+      case "-":
+        resultNum = oldNum - num;
+        break;
+
+      case "*":
+        resultNum = oldNum * num;
+        break;
+
+      case "/":
+        resultNum = oldNum / num;
+        break;
+
+      default:
+        resultNum = num;
+    }
+
+    if (!isFinite(resultNum)) {
+      resultNum = "Ошибка";
+    }
+
+    if (!display.value.includes(".")) {
+      console.log("Много точек");
+    }
+
+    display.value = parseFloat(resultNum.toFixed(2));
+    oldNum = 0;
+    num = resultNum;
+    operator = null;
+  }
+
+  equal.addEventListener("click", displayNum);
 }
-showNum();
 
-function onClearButton() {
-  const display = document.getElementById("display");
-  display.value = "";
-}
-
-function onRemoveButton() {
-  const display = document.getElementById("display");
-  display.value = display.value.slice(0, -1);
-}
-
-const clearButton = document.getElementById("clear");
-clearButton.addEventListener("click", onClearButton);
-
-const removeButton = document.getElementById("remove");
-removeButton.addEventListener("click", onRemoveButton);
+window.onload = calc;
