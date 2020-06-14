a5b360ec723817323ff5cc653a395501355ea392
honeybaey <honeybaey@gmail.com>
2020-06-14 20:34:01 +0300

    adding support for keyboard

--- index.js
+++ index.js
@@ -112,6 +112,62 @@ function calc() {
 
   // support for keyboard
   document.addEventListener("keydown", function (e) {
+    if (e.code === "Backspace") {
+      onRemoveButton();
+    }
+    if (e.code === "Delete") {
+      onClearButton();
+    }
+    if (e.code === "Equal") {
+      displayNum();
+    }
+    if (e.code === "Digit0" || e.code === "Numpad0") {
+      setNumsOnKeyboard("0");
+    }
+    if (e.code === "Digit1" || e.code === "Numpad1") {
+      setNumsOnKeyboard("1");
+    }
+    if (e.code === "Digit2" || e.code === "Numpad2") {
+      setNumsOnKeyboard("2");
+    }
+    if (e.code === "Digit3" || e.code === "Numpad3") {
+      setNumsOnKeyboard("3");
+    }
+    if (e.code === "Digit4" || e.code === "Numpad4") {
+      setNumsOnKeyboard("4");
+    }
+    if (e.code === "Digit5" || e.code === "Numpad5") {
+      setNumsOnKeyboard("5");
+    }
+    if (e.code === "Digit6" || e.code === "Numpad6") {
+      setNumsOnKeyboard("6");
+    }
+    if (e.code === "Digit7" || e.code === "Numpad7") {
+      setNumsOnKeyboard("7");
+    }
+    if (e.code === "Digit8" || e.code === "Numpad8") {
+      setNumsOnKeyboard("8");
+    }
+    if (e.code === "Digit9" || e.code === "Numpad9") {
+      setNumsOnKeyboard("9");
+    }
+    if (e.code === "NumpadDivide") {
+      setOpsOnKeyboard("/");
+    }
+    if (e.code === "NumpadMultiply") {
+      setOpsOnKeyboard("*");
+    }
+    if (e.code === "Minus" || e.code === "NumpadSubtract") {
+      setOpsOnKeyboard("-");
+    }
+    if (e.code === "Plus" || e.code === "NumpadAdd") {
+      setOpsOnKeyboard("+");
+    }
+    if (e.code === "Period" || e.code === "NumpadDecimal") {
+      setNumsOnKeyboard(".");
+    }
+  });
+
   equal.addEventListener("click", displayNum);
 }
 
